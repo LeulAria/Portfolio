8344709e4e5ffdb999ce505f0ef7fad374006b14
LeulAria <leulsegedtariku@gmail.com>
2022-04-11 05:17:22 +0300

    add tailwind dependencies

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  typescript: {
-    ignoreDevErrors: true,
-    ignoreBuildErrors: true,
-  },
 };
 
 module.exports = nextConfig;
