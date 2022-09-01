0e9c64fa2c4638b9e2f910137deefce5265a3910
LeulAria <leulsegedtariku@gmail.com>
2022-09-01 23:36:10 +0300

    portfolio dark

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Ripple from "react-waves-effect/lib";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
+    <div className="dark flex min-h-screen flex-col items-center justify-center">
       <Head>
         <title>Create Next App</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -19,14 +19,15 @@ const Home: NextPage = () => {
 
       <div className="hero__bg"></div>
 
-      <main className="bg-white text-black flex flex-col justify-center w-full min-h-[100vh] px-[20%]">
+      <main className=" bg-black text-white flex flex-col justify-center w-full min-h-[100vh] px-[20%]">
         <div className="flex flex-grow items-center justify-center">
-          <div className="basis-[80px] h-full">
+          <div className=" hidden lg:block basis-[80px] h-full text-white border-r">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="35px"
               height="35px"
               className="my-4"
+              fill="currentColor"
               viewBox="0 0 24 24"
             >
               <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
@@ -37,6 +38,7 @@ const Home: NextPage = () => {
               width="35px"
               height="35px"
               className="my-4"
+              fill="currentColor"
               viewBox="0 0 24 24"
             >
               <path d="M24 0l-6 22-8.129-7.239 7.802-8.234-10.458 7.227-7.215-1.754 24-12zm-15 16.668v7.332l3.258-4.431-3.258-2.901z" />
@@ -47,6 +49,7 @@ const Home: NextPage = () => {
               width="35px"
               height="35px"
               className="my-4"
+              fill="currentColor"
               viewBox="0 0 24 24"
             >
               <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
@@ -57,6 +60,7 @@ const Home: NextPage = () => {
               width="35px"
               height="35px"
               className="my-4"
+              fill="currentColor"
               viewBox="0 0 24 24"
             >
               <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
@@ -67,33 +71,37 @@ const Home: NextPage = () => {
               width="35px"
               height="35px"
               className="my-4"
+              fill="currentColor"
               viewBox="0 0 24 24"
             >
               <path d="M0 3v18h24v-18h-24zm21.518 2l-9.518 7.713-9.518-7.713h19.036zm-19.518 14v-11.817l10 8.104 10-8.104v11.817h-20z" />
             </svg>
           </div>
-          <div className="border-l pl-10">
-            <h1 className="mx-auto text-center text-6xl font-[900] font-body">
+
+          <div className="pl-0 md:pl-10 w-[90vw] mx-auto md:w-auto">
+            <h1 className="mx-auto text-center text-5xl md:text-6xl lg:text-7xl font-[900] font-body whitespace-nowrap">
               Hy i'm LeulAria
               <br />
               Software Engineer!
             </h1>
 
-            <p className="text-center mt-3 text-xl max-w-[550px] text-zinc-600">
+            <p className="text-center mt-3 text-sm md:text-xl mx-auto max-w-[550px] text-zinc-300">
               Web developer . React Developer . Typescript Developer . Node
               Developer . Flutter Developer . UI/UX . DevOps . Golang . Engineer
               . Blogger.
             </p>
 
             <div className="flex justify-center mt-10 mx-auto">
-              <Ripple>
-                <button className="mx-1 px-16 py-3 bg-black text-white rounded-[2rem]">
-                  Contact
-                </button>
-              </Ripple>
               <div className="overflow-hidden rounded-[2rem] mx-2">
-                <Ripple color="#888">
-                  <button className="overflow-hidden px-16 py-3 border border-black text-black rounded-[2rem]">
+                <Ripple color="#ddd">
+                  <button className="overflow-hidden px-10 py-2 md:px-16 md:py-3 bg-indigo-700 text-white rounded-[3rem]">
+                    Contact
+                  </button>
+                </Ripple>
+              </div>
+              <div className="overflow-hidden rounded-[2rem] mx-2">
+                <Ripple color="#ddd">
+                  <button className="overflow-hidden px-10 py-2 md:px-16 md:py-3 bg-[#333] text-white rounded-[3rem]">
                     Resume
                   </button>
                 </Ripple>
@@ -104,7 +112,7 @@ const Home: NextPage = () => {
         <p className="mt-auto mb-10 text-center">Powered by Vercel</p>
       </main>
 
-      <main className="relative bg-white text-black flex flex-col justify-center w-full min-h-[100vh] px-[20%]">
+      {/* <main className="relative bg-white dark:bg-black text-black flex flex-col justify-center w-full min-h-[100vh] px-[20%]">
         <div className="hero__bg-2 absolute top-[200vh]"></div>
         <div className="flex flex-grow items-center justify-center">
           <div className="basis-[80px] h-[90%]"></div>
@@ -137,8 +145,7 @@ const Home: NextPage = () => {
         <p className="mt-auto mb-10 text-center">Powered by Vercel</p>
       </main>
 
-      {/* 
-      <section className="flex flex-col justify-center  w-full min-h-[50vh] bg-[#fff] px-[10%]">
+      <section className="flex flex-col justify-center  w-full min-h-[50vh] bg-[#000] px-[white%]">
         <div className="mb-10">
           <h1 className="mt-0 text-center text-6xl font-[900] font-body">
             Experiences
@@ -189,9 +196,9 @@ const Home: NextPage = () => {
             <h4 className="-pt-10 text-sm">July, 2020 - Dec, 2021</h4>
           </div>
         </div>
-      </section>
+      </section> */}
 
-      <section className="flex flex-col justify-center w-full min-h-[90vh] bg-[#fff] text-black px-[10%]">
+      {/* <section className="flex flex-col justify-center w-full min-h-[90vh] bg-[#000] text-white px-[10%]">
         <div className="flex items-center justify-center">
           <div className="relative w-[250px] h-[300px] rounded-full border border-black overflow-hidden">
             <Image layout="fill" src="/imgs/me.jpg" />
@@ -217,7 +224,7 @@ const Home: NextPage = () => {
         </div>
       </section>
 
-      <section className="flex flex-col justify-center  w-full min-h-[50vh] bg-[#fff] px-[10%]">
+      <section className="flex flex-col justify-center  w-full min-h-[50vh] bg-[#000] px-[white%]">
         <div className="mb-10">
           <h1 className="mt-0 text-center text-5xl font-[900] font-body">
             My Skills
@@ -266,7 +273,7 @@ const Home: NextPage = () => {
         </div>
       </section>
 
-      <section className="flex flex-col justify-center  w-full min-h-[70vh] bg-[#fff] px-[10%]">
+      <section className="flex flex-col justify-center  w-full min-h-[70vh] bg-[#000] px-[10%]">
         <div className="mb-10">
           <h1 className="mt-0 text-center text-5xl font-[900] font-body">
             Contact
