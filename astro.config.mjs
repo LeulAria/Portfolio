c0d1ac31311269c86f7e05538d374cd18d9b43e3
LeulAria <leulsegedtariku@gmail.com>
2023-12-13 09:54:30 +0400

    init assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
 import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
+  adapter: vercel(),
   integrations: [tailwind(), sitemap(), robotsTxt(), react()],
   site: SITE_URL,
   markdown: {
